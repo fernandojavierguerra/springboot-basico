0beb4c08581ae76e9f9a90ba3a946298f737b7de
Javier Guerra <jguerra@Desktop-jguerra>
2017-03-03 09:59:52 -0300

    Inicial

---
+++ src/main/java/com/jguerra/SpringbootBasicoApplication.java
@@ -0,0 +1,12 @@
+package com.jguerra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicoApplication.class, args);
+	}
+}
